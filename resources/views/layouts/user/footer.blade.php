556a80b28ddc19472da5d4101a1274e6d8ef5537
Abdourahmane Diop <cabraldiop18@gmail.com>
2020-02-01 21:49:13 +0000

    fix bug composer

--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -28,7 +28,7 @@
             </div>
             <div class="col-md-6 col-sm-6 my-auto p-2">
                 <p class="text-center my-auto">
-                    &copy; Copyright <a href="{{ route('admin.welcome') }}" style="color: blanchedalmond" target="_blank"b>BBC University</b> </a> 2020 - {{ Date('Y') }} | Design by EMPRO
+                    &copy; Copyright <a href="{{ route('admin.welcome') }}" style="color: blanchedalmond" target="_blank"b>BBC University</b> </a> 2020 - {{ Date('Y') }} | Developed by EMPRO
 
                 </p>
             </div>
