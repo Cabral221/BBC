c38c4adcd9009266672be086471febc549cb092c
Abdourahmane Diop <cabraldiop18@gmail.com>
2020-02-12 20:18:47 +0000

    reafact for delete file

--- resources/views/emails/userAdmisson.blade.php
+++ resources/views/emails/userAdmisson.blade.php
@@ -11,7 +11,7 @@ Phone : {{ $request['phone'] }}<br>
 @component('mail::panel')
 Program : <b>{{ $request['program_id']->libele }}</b><br>
 Level : <b>{{ $request['niveau_id']->libele }}</b><br>
-Faculty : <b>{{ $request['filiere_id']->lebele }}</b><br>
+Faculty : <b>{{ $request['filiere_id']->libele }}</b><br>
 @endcomponent
 
 @component('mail::button', ['url' => env('APP_URL')])
