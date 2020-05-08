5a96c0cb65a6dcd5f42380f8ae2f0faea80dbf54
Abdourahmane Diop <cabraldiop18@gmail.com>
2020-05-08 01:10:34 +0000

    add Admin Class

--- app/Mail/messageCreated.php
+++ app/Mail/messageCreated.php
@@ -2,10 +2,11 @@
 
 namespace App\Mail;
 
+use App\Admin;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class messageCreated extends Mailable
 {
