d127fa8592a2a09dc9c92a0a9fc633f5f4c1f15b
Abdourahmane Diop <cabraldiop18@gmail.com>
2020-09-26 15:17:32 +0000

    reafct asset 404 failed for admin layouts

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  
+
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -17,13 +17,11 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   
   <!-- Custom styles for this template-->
-  <link href="{{asset('img')}}" rel="stylesheet">
-  <link href="{{asset('images')}}" rel="stylesheet">
   <link href="{{asset('asset_admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
   <link href="{{asset('asset_admin/css/sb-admin-2.css')}}" rel="stylesheet">
 
 {{-- <script src="https://cdn.ckeditor.com/ckeditor5/19.1.1/classic/ckeditor.js"></script> --}}
-<script src="https://cdn.ckeditor.com/ckeditor5/19.1.1/inline/ckeditor.js" defer></script>
+{{-- <script src="https://cdn.ckeditor.com/ckeditor5/19.1.1/inline/ckeditor.js" defer></script> --}}
   
 </head>
 
