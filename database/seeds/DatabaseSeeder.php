309dbb5f141e89e02c7c02401737933a79a78acf
Abdourahmane Diop <cabraldiop18@gmail.com>
2022-07-26 13:47:26 +0000

    Add other seeder in default file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
 
         // Seed du premier image comme slide
         Slide::create([
@@ -49,10 +48,14 @@ class DatabaseSeeder extends Seeder
         // Seed du compte d'administrateur
         Admin::create([
             'name' => "Philia Schiemen",
-            'email' => "philia@bbc-sn.com",
+            'email' => "admin@admin.com",
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ]);
+
+        $this->call(ModalSeeder::class);
+        $this->call(SlugSeeder::class);
+
     }
 }
